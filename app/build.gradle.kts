b8fe21a4a2d68e41e6d420f139ac1b4e5497a123
Yotam Gat <yotamgat2@gmail.com>
2025-03-16 18:42:17 +0200

    Project stracture

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.mykotlinblog"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.mykotlinblog"
